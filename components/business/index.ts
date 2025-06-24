5cd560558eb023ee6207cc4a18725e70ed5f1b88
MCerqua <55824402+MCERQUA@users.noreply.github.com>
2025-06-24 14:09:12 -0400

    Remove QuoteRequestForm from exports

--- components/business/index.ts
+++ components/business/index.ts
@@ -1,41 +1,19 @@
-// Business-specific components for local service businesses
-export { BusinessHours } from './BusinessHours'
-export type { BusinessHoursProps, DayHours } from './BusinessHours'
-
-export { EmergencyBanner } from './EmergencyBanner'
-export type { EmergencyBannerProps } from './EmergencyBanner'
-
-export { ServiceAreaMap } from './ServiceAreaMap'
-export type { ServiceAreaMapProps, ServiceArea } from './ServiceAreaMap'
-
-export { TestimonialCard } from './TestimonialCard'
-export type { TestimonialCardProps, Testimonial } from './TestimonialCard'
-
-export { BeforeAfterGallery } from './BeforeAfterGallery'
-export type { BeforeAfterGalleryProps, BeforeAfterImage } from './BeforeAfterGallery'
-
-export { TeamMember } from './TeamMember'
-export type { TeamMemberProps, TeamMemberSocial, TeamMemberCertification } from './TeamMember'
-
-// Contact and Form Components
+// Form Components
 export { ContactForm } from './ContactForm'
-export type { ContactFormProps, ContactFormData } from './ContactForm'
-
-export { ClickToCall } from './ClickToCall'
-export type { ClickToCallProps } from './ClickToCall'
-
-export { QuoteRequestForm } from './QuoteRequestForm'
-export type { QuoteRequestFormProps, QuoteRequestData } from './QuoteRequestForm'
-
-export { ServiceAreaChecker } from './ServiceAreaChecker'
-export type { ServiceAreaCheckerProps, ServiceAreaConfig } from './ServiceAreaChecker'
-
-// Form Field Components
+export type { ContactFormData, ContactFormProps } from './ContactForm'
 export { FormField } from './FormField'
-export type { FormFieldProps } from './FormField'
-
 export { TextAreaField } from './TextAreaField'
-export type { TextAreaFieldProps } from './TextAreaField'
-
 export { SelectField } from './SelectField'
-export type { SelectFieldProps, SelectOption } from './SelectField'
+export type { SelectOption } from './SelectField'
+
+// Business Components
+export { BusinessHours } from './BusinessHours'
+export { EmergencyBanner } from './EmergencyBanner'
+export { ServiceAreaChecker } from './ServiceAreaChecker'
+export { ServiceAreaMap } from './ServiceAreaMap'
+export { ClickToCall } from './ClickToCall'
+
+// Visual Components
+export { BeforeAfterGallery } from './BeforeAfterGallery'
+export { TeamMember } from './TeamMember'
+export { TestimonialCard } from './TestimonialCard'
